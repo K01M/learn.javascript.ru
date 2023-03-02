d62a07fefc1165cfe103ecc46c764f4565c9f45f
K01M <michail.kasyanov@yandex.ru>
2023-03-02 11:37:42 +0300

    ex49

---
+++ ex49/main.js
@@ -0,0 +1,22 @@
+// Создайте объект calculator (калькулятор) с тремя методами:
+
+//     read() (читать) запрашивает два значения и сохраняет их как свойства объекта.
+//     sum() (суммировать) возвращает сумму сохранённых значений.
+//     mul() (умножить) перемножает сохранённые значения и возвращает результат.
+
+let calculator = {
+  read() {
+    calculator.a = +prompt('Введите первое число');
+    calculator.b = +prompt('Введите второе число');
+  },
+  sum() {
+    return this.a + this.b;
+  },
+  mul() {
+    return this.a * this.b;
+  },
+};
+
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
