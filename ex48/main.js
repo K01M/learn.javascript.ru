307c21d3939d17e231cbd88b6911f03f6e52654b
K01M <michail.kasyanov@yandex.ru>
2023-03-02 11:30:48 +0300

    ex48+added done folder

---
+++ ex48/main.js
@@ -0,0 +1,10 @@
+// Здесь функция makeUser возвращает объект.
+// Каким будет результат при обращении к свойству объекта ref? Почему?
+// function makeUser() {
+//   return {
+//     name: "John",
+//     ref: this // тут вернет undefind
+//   };
+// }
+// let user = makeUser();0
+// alert( user.ref.name ); // Каким будет результат? // тут ошибку выдаст при попытке прочитать undefind
